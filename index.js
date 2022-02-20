deeaab6c0cdf0fee1e7ac6b1b3e379681fbd7cac
Daniel Vergara <danielvsuarez01@gmail.com>
2022-02-19 22:29:11 -0500

    routing with express

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ app.get('/', (req, res) => {
   res.send('My server in express');
 });
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola esta es una nueva ruta');
+});
+
+app.get('/productos', (req, res) => {
+  res.json({
+    name: 'Product',
+    price: 1500,
+  });
+});
+
+app.get('/categories', (req, res) => {
+  res.json({
+    name: 'Tools',
+  });
+});
+
 app.listen(port, () => {
   console.log('my port', port);
 });
