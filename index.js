cff0c0295c1231556a62a4adec5b8e6b9cc3ffb3
Daniel Vergara <danielvsuarez01@gmail.com>
2022-02-23 23:01:21 -0500

    other

--- index.js
+++ index.js
@@ -14,20 +14,24 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const whiteList = ['http://localhosts:8080', 'https://myapp.co'];
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
 const options = {
   origin: (origin, callback) => {
-    if (whiteList.includes(origin) || !origin) {
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     } else {
       callback(new Error('no permitido'));
     }
   },
 };
-
 app.use(cors(options));
+
 app.get('/', (req, res) => {
-  res.send('My server in express');
+  res.send('Hola mi server en express');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
 });
 
 routerApi(app);
@@ -38,5 +42,5 @@ app.use(boomErrorHanddler);
 app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log('my port', port);
+  console.log('Mi port' + port);
 });
